f8fd3482158fc1839d2edebc6a126a8128fba1e7
SnehAl207 <snehalsingh353@gmail.com>
2025-05-03 18:30:29 +0530

    Numpy Basics Matrixs complete

--- Numpy Matrix.py
+++ Numpy Matrix.py
@@ -7,3 +7,17 @@ print(n1[0])
 #to print a particular element, row etc
 print(n1[1:2,2])
 
+#transpase
+np.transpose(n1)
+print(n1)
+
+#using dot product to multiply matrixs
+n2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+n1 = np.array([[9,8,7],[6,5,4],[3,2,1]])
+
+print(n1.dot(n2))
+
+# save and load an array
+np.save('my_numpy',n1)
+n45 = np.load('my_numpy.npy')
+print(n45)
